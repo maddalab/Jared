307217eff53062c007559c370559bfddd9147edd
Bhaskar Maddala <bhaskar@tumblr.com>
2013-06-29 22:50:16 -0400

    Added test to create table with options

--- src/test/java/com/jamaav/jared/TableQueriesTest.java
+++ src/test/java/com/jamaav/jared/TableQueriesTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jamaav.jared.CreateTableOptions.Durability;
 import com.jamaav.jared.db.Connection;
 import com.jamaav.jared.db.DriverManager;
 
@@ -38,6 +39,23 @@ public class TableQueriesTest {
       String[] tables = Rethink.r(c).database("superheroes").listTables();
       Assert.assertEquals(1, tables.length);
       Assert.assertEquals("marvel", tables[0]);
+    } finally {
+      c.close();
+    }
+  }
+
+  @Test
+  public void testCreateTableWithPrimaryKey() throws ConnectionException,
+      QueryException {
+    CreateTableOptions options = new CreateTableOptions();
+    options.setPrimaryKey("name");
+    options.setCacheSize(128);
+    options.setDurability(Durability.SOFT);
+    Connection c = DriverManager.getConnection("localhost", 28015);
+    Rethink r = Rethink.r(c).database("superheroes");
+    try {
+      r.createTable("dc", options);
+      r.dropTable("dc");
     } finally {
       c.close();
     }
